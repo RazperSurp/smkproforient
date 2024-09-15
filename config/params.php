29716b24d885ac063b899515c09d92bbbffc0e8b
RazperSurp <dshiyanov01@gmail.com>
2024-09-15 11:31:05 +0300

    v0.0.1 init

---
+++ config/params.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'adminEmail' => 'admin@example.com',
+    'senderEmail' => 'noreply@example.com',
+    'senderName' => 'Example.com mailer',
+    'version' => 
+    [ 
+        'num' => '0.0.1',
+        'name' => 'init'
+    ]
+];
